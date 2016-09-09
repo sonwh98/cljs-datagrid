8d6d2978bd9af75c16fb804858e7488a2c9ae88a
sto <Son.C.To@gmail.com>
2016-09-09 17:06:05 -0500

    change project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-datagrid "0.1.0-SNAPSHOT"
+(defproject com.kaicode/datagrid "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
