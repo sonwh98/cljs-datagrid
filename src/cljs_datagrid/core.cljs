d5e06d880db0a4a6dd42f4ec4e2e4c0a3958482c
sto <Son.C.To@gmail.com>
2016-09-09 14:27:00 -0500

    refactor

--- src/cljs_datagrid/core.cljs
+++ src/cljs_datagrid/core.cljs
@@ -1,32 +1,26 @@
 (ns cljs-datagrid.core
-  (:require [reagent.core :as reagent :refer [atom]]
+  (:require [reagent.core :as reagent]
             [com.kaicode.datagrid :as data-grid]))
 
 (enable-console-print!)
 
-(println "This text is printed from src/cljs-datagrid/core.cljs. Go ahead and edit it and see reloading in action.")
-
-;; define your app data so that it doesn't get over-written on reload
-
 (defn init-db []
   (let [weight        (/ 1 3)
         common-config {:visible      true
-                       ;:save-fn      cell/save-fn
                        :width-weight weight}
         people        [{:person/first-name "Sonny"
                         :person/last-name  "Su"
                         :person/email      "sonny.su@foobar.com"}
                        {:person/first-name "John"
                         :person/last-name  "Smith"
-                        :person/email      "john.smith@foobar.com"}]
+                        :person/email      "john.smith@foobar.com"}
+                       {:person/first-name "Jane"
+                        :person/last-name "Doe"
+                        :person/email "jane.doe@foobar.com"}]
         app-state     (reagent/atom {:window-dimension {:width  (. js/window -innerWidth)
                                                         :height (. js/window -innerHeight)}
-                                     :catalog          people
                                      :rows             people
-                                     :search-fn        nil
                                      :on-delete-rows   nil
-                                     :sort-column      {:none true}
-                                     :selected-rows    #{}
                                      :columns-config   [[:person/first-name (merge common-config {:unique           true
                                                                                                   :render-header-fn (constantly "First Name")})]
                                                         [:person/last-name (merge common-config {:render-header-fn (constantly "Last Name")})]
