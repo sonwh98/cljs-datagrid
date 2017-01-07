d23ee86a54f96c2234e730089a01a9a5e7e5389b
Piotr Tarasewicz <tarasewiczp@gmail.com>
2017-01-07 01:39:19 +0100

    correctly set z-index of context-menu

--- src/com/kaicode/datagrid.cljs
+++ src/com/kaicode/datagrid.cljs
@@ -419,7 +419,7 @@
                      :border           "1px solid grey"
                      :padding          5
                      :position         :absolute
-                     :z-index          1
+                     :z-index          1000
                      :left             (first coordinate)
                      :top              (second coordinate)}}
        content])))
